6010c242523c2ac5d98039bd547b51d4cc39b786
Anastasia Anferova <anf0404@bk.ru>
2022-04-14 20:49:01 +0400

    Add completed task 01

--- task01/task01.ino
+++ task01/task01.ino
@@ -1,7 +1,30 @@
-void setup() {
+int ledLights[] = {3, 5, 6, 9, 10};
+unsigned long intervals[] = {300000, 200000, 40000, 200, 40};
 
+int ledStates[] = {LOW, LOW, LOW, LOW, LOW};
+unsigned long milliseconds[] = {0, 0, 0, 0, 0};
+
+void setup() {
+  for (int ledN = 0; ledN < 5; ledN++)
+  {
+    pinMode(ledLights[ledN], OUTPUT);
+  }
 }
 
 void loop() {
+  for (int ledN = 0; ledN < 5; ledN++)
+  {
+    unsigned long currentMilliseconds = micros();
+    if (currentMilliseconds - milliseconds[ledN] >= intervals[ledN]) {
+      milliseconds[ledN] = currentMilliseconds;
 
+      if (ledStates[ledN] == LOW) {
+        ledStates[ledN] = HIGH;
+      } 
+      else {
+        ledStates[ledN] = LOW;
+      }
+       digitalWrite(ledLights[ledN], ledStates[ledN]);
+    }
+  }
 }
